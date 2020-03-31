006b5efddbf0889efc79eb209a279aa87d667077
Marcos Pinho <mpinho@localhost.localdomain>
2020-03-31 20:56:57 -0300

    Adaptações no conky

---
+++ conky-xanimos/conky_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+killall conky
+conky -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_bg &
+conky -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_clock &
+conky -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_txt &
+conky -p 3 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_files &
+conky -p 6 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_system &
+conky -p 4 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_network &
+conky -p 5 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_cpu &
+conky -p 7 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_ram
+
+exit
