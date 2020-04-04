51d4435c2c29c63a0540e2af86dc3dc5cd3e7ad8
Marcos Pinho <marcosjava2008@gmail.com>
2020-04-03 22:23:28 -0300

    Adicionando metereologia na monitoria

--- conky-xanimos/conky_start.sh
+++ conky-xanimos/conky_start.sh
@@ -7,6 +7,7 @@ conky -p 3 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xani
 conky -p 6 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_system &
 conky -p 4 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_network &
 conky -p 5 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_cpu &
-conky -p 7 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_ram
+conky -p 7 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_ram &
+conky -p 8 -c /home/mpinho/Desenvolvimento/conky/versoes/custom-conky/conky-xanimos/configs/conky_climate
 
 exit
